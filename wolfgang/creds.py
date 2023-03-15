2d425e055a181bbc2169301e2b9b303f8bb3b780
Aly Kapasi <122089907+alykapasi@users.noreply.github.com>
2023-03-14 20:38:12 -0700

    first push basic content

---
+++ wolfgang/creds.py
@@ -0,0 +1,3 @@
+FILE_PATH = "sqlite:///tracklist.sqlite"
+USER_ID = "aly.kapasi"
+AUTH_TOKEN = "" # fill in later
---
+++ wolfgang/extract.py
@@ -0,0 +1,14 @@
+## (E)xtract from spotify REST api
+# import relevant libraries
+import pandas as pd
+import sqlalchemy
+import sqlite3
+import json
+import requests
+import datetime
+from sqlalchemy.orm import sessionmaker
+from datetime import datetime
+
+import creds
+
+if __name__ == "__main__":
